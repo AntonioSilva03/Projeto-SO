7430974f79e9c4cf113a277ff57e420e8e6c18cf
Pedrosilva03 <pedroo.silva180@gmail.com>
2024-03-16 02:44:22 +0000

    Defines e algumas coisas no cliente

--- src/client/client.c
+++ src/client/client.c
@@ -1,3 +1,27 @@
-int main(){
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "../defines/defines.h"
+
+int main(int argc, char *argv[]){
+    int fd_pipe;
+    if(argc < 2){
+        write(STDERR_FILENO, BOOT_ERROR, sizeof(BOOT_ERROR));
+    }
+
+    mkfifo("/tmp/pipe", 0666);
+
+    char* command = argv[1];
+    if(strcmp(command, "execute") == 0){
+
+    }
+    else if(strcmp(command, "status") == 0){
+
+    }
+    else{
+
+    }
     return 0;
 }
---
+++ src/defines/defines.h
@@ -0,0 +1,1 @@
+#define BOOT_ERROR "Insert command to execute\n"
